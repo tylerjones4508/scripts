6bfea167378f5e42992d176db3a823e4cfc52765
tyler jones <Tyler.Jones@sterlingts.com>
2016-08-17 14:57:06 -0400

    Master bootstrap

---
+++ master-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SCRIPT TO INSTALL SALT-Master LINUX RHEL OR Debian/Ubuntu
+#Usage: This is to be used with SSH to execute the install over ssh. 
+#Command: ssh user@hostname 'bash -s' < /path/to/master-install.sh
+
+
+curl -L https://bootstrap.saltstack.com -o sudo sh install_salt.sh -P -M
+
